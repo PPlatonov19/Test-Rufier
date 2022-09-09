938ecaf1d90eb3be84dc7cce25f6a957e6acdc2c
PPlatonov19 <petr.platonov2006@gmail.com>
2022-09-09 09:49:11 +0300

    asd

--- my app.py
+++ my app.py
@@ -1,5 +1,6 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QHBoxLayout, QVBoxLayout, QRadioButton, QMessageBox
+from instr import *
 class FScreen(QWidget):
     def __init__(self):
         super().__init__()
@@ -27,9 +28,40 @@ class FScreen(QWidget):
 
     def next_win(self):
         self.hide()
-        #создать класс следующего экрана
+        
+class SScreeen(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
 
-#class SScreeen(QWidget):
+    def set_appear(self):
+        self.setWindowTitle(text_title)
+        self.reisze(win_width, win_height)
+
+    def initUI(self):
+        self.instruction1 = QLabel(text_instruction1)
+        self.instruction2 = QLabel(text_instruction2)
+        self.instruction3 = QLabel(text_instruction3)
+        self.instruction4 = QLabel(text_instruction4)
+        self.instruction5 = QLabel(text_instruction5)
+        self.button = QPushButton(text_button)
+        self.VLayout = QVBoxLayout()
+        self.VLayout.addWidget(self.instruction1)
+        self.VLayout.addWidget(self.instruction2)
+        self.VLayout.addWidget(self.instruction3)
+        self.VLayout.addWidget(self.instruction4)
+        self.VLayout.addWidget(self.instruction5)
+        self.VLayout.addWidget(self.button)
+        self.setLayout(VLayout)
+
+    def connects(self):
+        self.button.clicked.connect(self.final_win)
+
+    def next_win(self):
+        self.hide()
 
 app = QApplication([])
 mw = FScreen()
